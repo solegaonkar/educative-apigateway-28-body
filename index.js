885ab00c2d54f3f4fd6321a8d5fb550bfc955bc9
Vikas Solegaonkar <vikas.solegaonkar@gmail.com>
2022-08-01 18:28:19 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * This code is deployed as the Lambda function in AWS. 
+ * It just returns the simple string Hello World.
+ */
+
+exports.handler = async(event, context) => {
+  return event;
+};
+
